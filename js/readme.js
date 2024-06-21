fa007d04e91347a670b0ae1325e4c9c0d149dc93
leedohyung28 <dhleee0123@gmail.com>
2024-06-21 14:39:14 +0900

    tmp

--- js/readme.js
+++ js/readme.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const timelineContents = document.querySelectorAll(".timeline-content");
   const profileContainer = document.querySelector(".col-9.text-white");
+  let readmeMarginTop;
 
   timelineContents.forEach((box, index) => {
     box.addEventListener("click", () => {
@@ -24,21 +25,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (index == 3) {
           readmeContainer.appendChild(osSimulator);
+          readmeMarginTop = "550px";
         } else if (index == 5) {
           readmeContainer.appendChild(exchangeGermany);
+          readmeMarginTop = "1050px";
         } else if (index == 8) {
           readmeContainer.appendChild(anythink);
+          readmeMarginTop = "1500px";
         } else if (index == 11) {
           readmeContainer.appendChild(roadAnalysis);
+          readmeMarginTop = "2200px";
         }
         readmeContainer.classList.add("balloon");
 
-        const restoreContainer = document.getElementById("restoreContainer");
+        const rightArrowContainer = document.getElementById("right-arrow-container");
+        rightArrowContainer.style.width = "100%"
         const btnMin = readmeContainer.querySelector(".btn-min");
         if (btnMin) {
           btnMin.addEventListener("click", () => {
             readmeContainer.classList.remove("balloon");
             readmeContainer.classList.add("slide-out-right");
+            console.log(readmeMarginTop)
 
             // 애니메이션이 끝난 후 요소 제거
             readmeContainer.addEventListener(
@@ -51,19 +58,20 @@ document.addEventListener("DOMContentLoaded", () => {
                   ? readmeTitle.textContent
                   : "Restored Container";
 
-                restoreContainer.classList.add("visible");
-                restoreContainer.innerHTML = `${titleText} <span>&#9654;</span>`; // 화살표 추가
+                rightArrowContainer.classList.add("visible");
+                rightArrowContainer.style.marginTop = readmeMarginTop;
+                rightArrowContainer.innerHTML = `${titleText} <span>&#9654;</span>`; // 화살표 추가
               },
               { once: true }
             );
           });
         }
-        restoreContainer.addEventListener("click", () => {
+        rightArrowContainer.addEventListener("click", () => {
           readmeContainer.style.display = "block"; // 요소를 다시 보이게 합니다.
           readmeContainer.classList.remove("slide-out-right");
           readmeContainer.classList.add("balloon"); // 원래 상태로 복원
-          restoreContainer.classList.remove("visible");
-          restoreContainer.innerHTML = ""; // 화살표와 텍스트 제거
+          rightArrowContainer.classList.remove("visible");
+          rightArrowContainer.innerHTML = ""; // 화살표와 텍스트 제거
         });
       } else {
         readmeContainer.classList.add("readme-left");
@@ -72,24 +80,36 @@ document.addEventListener("DOMContentLoaded", () => {
         profileContainer.insertAdjacentElement("beforebegin", readmeContainer);
         if (index == 4) {
           readmeContainer.appendChild(bcsdMobile);
+          readmeMarginTop = "640px";
         } else if (index == 6) {
           readmeContainer.appendChild(powerBI);
+          readmeMarginTop = "970px";
         } else if (index == 7) {
           readmeContainer.appendChild(bcsdGame);
+          readmeMarginTop = "1300px";
         } else if (index == 9) {
           readmeContainer.appendChild(eipCertification);
+          readmeMarginTop = "1505px";
         } else if (index == 10) {
           readmeContainer.appendChild(hanaField);
+          readmeMarginTop = "1680px";
         } else if (index == 12) {
           readmeContainer.appendChild(koreatechour);
+          readmeMarginTop = "1880px";
         } else if (index == 13) {
           readmeContainer.appendChild(playball);
+          readmeMarginTop = "2060px";
         } else if (index == 14) {
           readmeContainer.appendChild(instaGenerator);
+          readmeMarginTop = "2350px";
         } else if (index == 15) {
           readmeContainer.appendChild(webFullstack);
+          readmeMarginTop = "2640px";
         }
         readmeContainer.classList.add("balloon");
+        const leftArrowContainer = document.getElementById("left-arrow-container");
+        leftArrowContainer.style.textAlign = "right";
+        leftArrowContainer.style.width = "100%";
 
         const btnMin = readmeContainer.querySelector(".btn-min");
         if (btnMin) {
@@ -101,12 +121,29 @@ document.addEventListener("DOMContentLoaded", () => {
             readmeContainer.addEventListener(
               "animationend",
               () => {
-                readmeContainer.remove();
+                readmeContainer.style.display = "none"; // 요소를 숨깁니다.
+                const readmeTitle =
+                  readmeContainer.querySelector("#readmeTitle");
+                const titleText = readmeTitle
+                  ? readmeTitle.textContent
+                  : "Restored Container";
+
+                leftArrowContainer.classList.add("visible");
+                leftArrowContainer.style.marginTop = readmeMarginTop;
+                leftArrowContainer.innerHTML = `<span>&#9664;</span> ${titleText}`; // 화살표 추가
               },
               { once: true }
             );
           });
         }
+
+        leftArrowContainer.addEventListener("click", () => {
+          readmeContainer.style.display = "block"; // 요소를 다시 보이게 합니다.
+          readmeContainer.classList.remove("slide-out-right");
+          readmeContainer.classList.add("balloon"); // 원래 상태로 복원
+          leftArrowContainer.classList.remove("visible");
+          leftArrowContainer.innerHTML = ""; // 화살표와 텍스트 제거
+        });
       }
       const btnClose = readmeContainer.querySelector(".btn-close");
       if (btnClose) {
